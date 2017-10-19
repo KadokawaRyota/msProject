0eb4c1e82341da4d2ffa0715a93ea7a629a55a6a
maead-ryou <maedax4649@yahoo.co.jp>
2017-10-19 16:57:26 +0900

    仮のタイトル作成

---
+++ Assets/Scripts/C#/TitleFlashing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleFlashing : MonoBehaviour {
+
+    public Text TitleFlash;
+    
+
+
+    bool Flag;
+    float Color;
+
+    // Use this for initialization
+    void Start()
+    {
+        Color = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //テキストの透明度を変更する
+        TitleFlash.color = new Color(0, 0, 0, Color);
+        if (Flag)
+            Color -= Time.deltaTime;
+        else
+            Color += Time.deltaTime;
+        if (Color < 0)
+        {
+            Color = 0;
+            Flag = false;
+        }
+        else if (Color > 1)
+        {
+            Color = 1;
+            Flag = true;
+        }
+    }
+}
