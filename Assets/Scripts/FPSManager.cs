8d525296e38366caf66ce845486df1f45cc64f4a
hashimotoamane <hashimoto.amane0311@gmail.com>
2017-10-23 20:49:02 +0900

    プレイヤーのアウトライントゥーンシェーダを実装 ミッション開始ボタンを実装

---
+++ Assets/Scripts/FPSManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSManager : MonoBehaviour {
+
+	[SerializeField]
+	Text fpsText;
+
+	[SerializeField]
+	int setFPS;
+	
+	int frameCount;
+	float prevTime;
+	float fps;
+	// Use this for initialization
+	void Start () {
+		//DontDestroyOnLoad(gameObject);
+		Application.targetFrameRate = setFPS;
+
+		frameCount = 0;
+		prevTime = 0.0f;
+		fps = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		++frameCount;
+
+		float time = Time.realtimeSinceStartup - prevTime;
+
+		if(time >= 0.5f)
+		{
+			fps = (frameCount / time);
+			fpsText.text = fps.ToString();
+
+			frameCount = 0;
+			prevTime = Time.realtimeSinceStartup;
+		}
+	}
+}
