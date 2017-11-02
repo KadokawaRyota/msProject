3a71379b815ae1d10ceacdb60406487aa39e8e3d
DESKTOP-TNBNJ7R\ao102 <ao102902kazu@gmail.com>
2017-11-02 12:40:05 +0900

    カメラ、プレイヤー移動速度など

--- Assets/Scripts/C#/Camera/OfflineCameraController.cs
+++ Assets/Scripts/C#/Camera/OfflineCameraController.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class OfflineCameraController : MonoBehaviour
 {
-	public GameObject targetObj;
-	private Vector3 targetPos;
-	private Quaternion targetRot;
-	public OfflinePostureController postureController;
-	private Vector3 cameraDir;
-	private Vector3 playerToCamera;
-	static Vector3 localPos;
+	public GameObject targetObj;                        // プレイヤー情報
+    public OfflinePostureController postureController;  // プレイヤー姿勢の
+    public Vector3 localPos;                            // カメラローカル位置
+
+    private Vector3 targetPos;                          // プレイヤー位置情報
+	private Quaternion targetRot;                       // プレイヤー角度情報
+	private Vector3 cameraDir;                          // カメラの進行方向射影ベクトル
+	private Vector3 playerToCamera;                     // プレイヤーからカメラへのベクトル
+    private Quaternion rotCamera;                       // カメラの傾き情報
+    public float rotPitch;                              // カメラのピッチ量
+
+    public Vector3 GetCameraDirection
+    {
+        get { return this.cameraDir; }
+    }
 
 	void Start()
 	{
 		cameraDir = Vector3.Scale(transform.forward, new Vector3(1, 1, 1)).normalized;
-
-		// 球面座標系中心点からプレイヤーまでの距離
-		localPos = new Vector3(0.0f, 0.3f, -0.5f);
 	}
 
 	void Update()
@@ -31,23 +36,57 @@ public class OfflineCameraController : MonoBehaviour
 
 		// カメラの姿勢をプレイヤーと同期
 		cameraDir = Vector3.ProjectOnPlane(cameraDir, postureController.GetsurfaceNormal);
-		transform.rotation = Quaternion.LookRotation(cameraDir, postureController.GetsurfaceNormal);
+        //transform.rotation = Quaternion.LookRotation(cameraDir, postureController.GetsurfaceNormal);
+        rotCamera = Quaternion.LookRotation(cameraDir, postureController.GetsurfaceNormal);
 
+        // 射影ベクトルで求めたカメラの傾きを退避
+        Quaternion rotCameraEvac = rotCamera;
 
+        // カメラのピッチ量に合わせて傾きを変更
+        rotCamera.x += rotPitch;
+        transform.rotation = rotCamera;
 
-		transform.position = targetObj.transform.position + transform.rotation * localPos;
+        // カメラ位置の調整
+        transform.position = targetObj.transform.position + rotCameraEvac * localPos;
+        //transform.position = targetObj.transform.position + rotCamera * localPos;
 
-		// マウスの右クリックを押している間
-		if (Input.GetMouseButton(1))
+        // カメラをX軸を中心に上下に向ける
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotPitch += Time.deltaTime * 0.10f;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotPitch -= Time.deltaTime * 0.10f;
+        }
+
+        // マウスの右クリックを押している間
+        if (Input.GetMouseButton(1))
 		{
 			// マウスの移動量
 			float mouseInputX = Input.GetAxis("Mouse X");
 			float mouseInputY = Input.GetAxis("Mouse Y");
 
-			// targetの位置のY軸を中心に、回転（公転）する
-			transform.RotateAround(targetPos, postureController.GetsurfaceNormal, mouseInputX * Time.deltaTime * 200f);
-			cameraDir = Vector3.ProjectOnPlane(targetPos - transform.position, postureController.GetsurfaceNormal);
+            // targetの位置のY軸を中心に、回転（公転）する
+            // playerToCamera = transform.position - targetPos;
+            // Vector3 proj;                                      // playerToCameraの法線に対する射影ベクトル
 
-		}
+            // proj = Vector3.Project(playerToCamera, postureController.GetsurfaceNormal);
+            // proj += targetPos;
+
+            //transform.RotateAround(targetPos, postureController.GetsurfaceNormal, mouseInputX * Time.deltaTime * 200f);
+            transform.RotateAround(targetPos, postureController.GetsurfaceNormal, mouseInputX * Time.deltaTime * 200f);
+            cameraDir = Vector3.ProjectOnPlane(targetPos - transform.position, postureController.GetsurfaceNormal);
+
+            if (rotPitch > 1.0f)
+            {
+                rotPitch = 1.0f;
+            }
+            else if (rotPitch < -1.0f)
+            {
+                rotPitch = -1.0f;
+            }
+
+        }
 	}
 }
