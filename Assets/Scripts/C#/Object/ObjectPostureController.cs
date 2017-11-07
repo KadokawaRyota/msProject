f9a9e00a6a9b048354cbe1a0876cf3694421609a
DESKTOP-TNBNJ7R\ao102 <ao102902kazu@gmail.com>
2017-11-07 15:52:09 +0900

    オブジェクト用整地の型地

---
+++ Assets/Scripts/C#/Object/ObjectPostureController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPostureController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
