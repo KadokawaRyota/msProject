f87005728451f23f3fb82fcc0c6784529eb59029
hashimotoamane <hashimoto.amane0311@gmail.com>
2017-11-30 11:14:35 +0900

    街灯のライトエフェクトの追加とマテリアル調整

---
+++ Assets/Scripts/C#/Other/Billboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ビルボード
+public class Billboard : MonoBehaviour {
+
+	void Update () {
+
+        //カメラポジションの取得
+        Vector3 pos = Camera.main.transform.forward;
+
+        //書き換えてその方向へ向かせる
+        pos.y = transform.position.y;
+        transform.LookAt(pos);
+    }
+}
