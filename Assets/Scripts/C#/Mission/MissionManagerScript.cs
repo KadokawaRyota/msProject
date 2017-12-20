7e9825ac7c6cc29c4368e396fe17a74fb756925e
hashimotoamane <hashimoto.amane0311@gmail.com>
2017-12-21 02:12:48 +0900

    offline消えてよくわかんねーからパッケージで直した

--- Assets/Scripts/C#/Mission/MissionManagerScript.cs
+++ Assets/Scripts/C#/Mission/MissionManagerScript.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class MissionManagerScript : MonoBehaviour {
+
+public class MissionManagerScript : MonoBehaviour
+{
 
     bool missionFlg = false;     //フラグの初期化
+    bool missionEndFlg = false; //終了フラグ（trueのときにリザルトフラグを立てる）
 
     [SerializeField]
     GameObject MissionCanvas;
@@ -13,15 +15,44 @@ public class MissionManagerScript : MonoBehaviour {
     [SerializeField]
     GameObject MissionType;
 
-	// Use this for initialization
-	void Start () {
+    //上バー関連
+    [SerializeField]
+    int missionStartCount = 30; //BG出現までの待機時間
+    int missionUICount = 0;
+    bool UIFlg = false;
+
+    [SerializeField]
+    Image missionBg;
+
+    //タイム関連
+    [SerializeField]
+    GameObject timer;
+
+    // Use this for initialization
+    void Start()
+    {
         missionFlg = false;     //フラグの初期化
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//ミッションを決めたりする処理をここに入れる予定
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ミッションを決めたりする処理をここに入れる予定
+
+        if (missionFlg)
+        {
+            if (!UIFlg)
+            {
+                if (missionUICount > missionStartCount)
+                {
+                    UIFlg = true;
+                    missionBg.GetComponent<Animator>().SetTrigger("open");
+                    return;
+                }
+                missionUICount++;
+            }
+        }
+    }
 
     public void StartMission()
     {
