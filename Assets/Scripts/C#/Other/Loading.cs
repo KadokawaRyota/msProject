856562ae1f36beb1a69745f2081d31ff20733994
hashimotoamane <hashimoto.amane0311@gmail.com>
2017-12-22 04:36:56 +0900

    細かいところいろいろ追加 ・ロード中の画面 ・タイトルタッチを全画面に対応 ・ResultSceneの作成 ・オンライン時に名前を表示（仮でタヌキのみ） ・チュートリアルを終了しているか判別するシングルトンを配置 ・画面比調整（たぶん直した） ・オンラインにもオクルージョンカリングを実装

---
+++ Assets/Scripts/C#/Other/Loading.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour {
+
+    [SerializeField]
+    Text loadTimeText;
+
+    [SerializeField]
+    int addTime = 30;
+
+    int cnt = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (cnt == addTime)
+        {
+            loadTimeText.text = ".";
+        }
+
+        else if (cnt == addTime * 2)
+        {
+            loadTimeText.text = "..";
+        }
+
+        else if (cnt == addTime * 3)
+        {
+            loadTimeText.text = "...";
+        }
+
+        else if(cnt == addTime * 4)
+        {
+            loadTimeText.text = "";
+            cnt = 0;
+            return;
+        }
+
+        cnt ++;
+	}
+}
---
+++ Assets/Scripts/C#/Player/PlayerName.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour {
+
+    [SerializeField]
+    Text nameText;      //NameTextUI
+
+    [SerializeField]
+    GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+
+        CharactorInfo charaInfo = GameObject.Find("CharactorInfo").GetComponent<CharactorInfo>();
+
+        //名前設定
+        if (null != charaInfo)
+        {
+            nameText.text = charaInfo.GetPlayerName();
+        }
+        else
+        {
+            nameText.text = "None";
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.rotation = camera.gameObject.transform.rotation;
+	}
+
+    public void SetNameText(string name)
+    {
+        nameText.text = name;
+    }
+}
